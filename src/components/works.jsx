28cbe28da5676afd011e3e59faad24da28ed55bf
Aliza Khan <alizaadi2@gmail.com>
2024-09-07 01:39:46 +0500

    Update works.jsx

--- src/components/works.jsx
+++ src/components/works.jsx
@@ -1,13 +1,13 @@
-const AppWork = ({workImage, imgHeading, imgPara}) => {
+const AppWork = ({ workImage, imgHeading, imgPara }) => {
     return (
-        <div class="card mb-5" style={{border:"none"}}>
-        <img src={workImage} class="card-img-top" alt="..."/>
-        <div class="card-body" style={{textAlign:"center", color:"#fff"}} >
-          <h5 class="card-title">{imgHeading}</h5>
-          <p class="card-text">{imgPara}</p>
-        </div>
-      </div>
-    )
+        <div class="card mb-5" style={{ border: "none" }}>
+            <img src={workImage} class="card-img-top" alt="..." />
+            <div class="card-body" style={{ textAlign: "center", color: "#fff" }} >
+                <h5 class="card-title">{imgHeading}</h5>
+                <p class="card-text">{imgPara}</p>
+            </div>
+    </div>
+            )
 }
 
-export default AppWork;
+            export default AppWork;
