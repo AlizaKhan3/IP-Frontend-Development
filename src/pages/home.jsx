3911715d411e0ca404779780184b1f61dc9f8de6
Aliza Khan <alizaadi2@gmail.com>
2024-09-06 22:39:24 +0500

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,12 @@
 import IndividualIntervalsExample from "../components/carousel";
+import Content from "../components/content";
+
 const Home = () => {
     return(
         <div>
            <IndividualIntervalsExample/>
+           <Content/>
+
         </div>
     )
 }
