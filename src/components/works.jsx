dea8d6aca05ee40fb170722e386d8fc3590ab01f
Aliza Khan <alizaadi2@gmail.com>
2024-09-07 00:19:52 +0500

    Update works.jsx

--- src/components/works.jsx
+++ src/components/works.jsx
@@ -1,23 +1,12 @@
-import userImg1 from "../assets/Mask (1).png"
-import userImg2 from "../assets/Mask.png"
-import userImg3 from "../assets/Userpic (1).png"
-import userImg4 from "../assets/Userpic.png"
-
-const AppWork = () => {
+const AppWork = ({workImage, imgHeading, imgPara}) => {
     return (
-        <div class="card mb-3 p-4">
-            <div class="row g-0">
-                <div class="col-md-2">
-                    <img src={userImg1} class="img-fluid rounded-start" alt="..." />
-                </div>
-                <div class="col-md-8">
-                    <div class="card-body" style={{color:"#fff"}}>
-                        <p class="card-text">Get a fully retina ready site when you build with Startup Framework. Websites look sharper and more gorgeous on devices with retina display support</p>
-                        <p class="card-text">RAYHAN CURRAN</p>
-                    </div>
-                </div>
-            </div>
+        <div class="card mb-5" style={{border:"none"}}>
+        <img src={workImage} class="card-img-top" alt="..."/>
+        <div class="card-body" style={{textAlign:"center", color:"#fff"}} >
+          <h5 class="card-title">{imgHeading}</h5>
+          <p class="card-text">{imgPara}</p>
         </div>
+      </div>
     )
 }
 
