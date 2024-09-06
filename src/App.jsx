753f048298f5b8a8dcd6265602918c7130a79831
Aliza Khan <alizaadi2@gmail.com>
2024-09-06 22:39:40 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Blog from './pages/blog';
 import Features from './pages/features';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Price from './pages/pricing'
+import Signup from './pages/signup';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
        <Route path="/pricing" element={<Price/>} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/features" element={<Features/>} />
+        <Route path='/signup' element={<Signup/>} />
       </Routes>
     </Router>
 
